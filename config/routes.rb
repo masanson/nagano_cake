56a12e2ec41427a193ada797c9891fb1876f7116
masanson <138214683+masanson@users.noreply.github.com>
2023-08-23 10:43:06 +0000

    [Update] cp4-1 管理者ログイン(途中2)

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
+  
+  get 'admin' => 'admin/homes#top', as: 'admin_top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
