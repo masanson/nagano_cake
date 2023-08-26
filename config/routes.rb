bfaf26fbdde2d764233cbcbc4f9ccbbf72bf6bae
masanson <138214683+masanson@users.noreply.github.com>
2023-08-26 12:59:50 +0000

    [Update] customers/contoroller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,24 @@
 Rails.application.routes.draw do
-  
+
   root to: "public/homes#top"
   get '/about' => 'public/homes#about', as: 'about'
-  
+
   devise_for :admin, skip:[:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  
+
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
-  
+
   namespace :public do
-    get 'items/index'
-    get 'items/show'
+    resources :items, only: [:index, :show]
+    resources :customers, only: [:show, :edit, :update]
+    get 'customers/withdrawal' => 'customers#withdrawal'
+    patch 'customers/withdrawal' => 'customers#update_wd'
   end
-  
+
   namespace :admin do
     get '' => 'homes#top', as: 'top'
   end
