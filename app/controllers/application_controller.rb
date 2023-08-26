28edab2dad1705db3ebcee34dcdfbb7a6008a7be
masanson <138214683+masanson@users.noreply.github.com>
2023-08-26 12:22:05 +0000

    [Update] customersign_up&log_in作成2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,6 +23,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telerhone_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number])
   end
 end
