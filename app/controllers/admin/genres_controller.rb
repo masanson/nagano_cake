5e4e584388f787434c269b99f0e67b7275771c99
masanson <138214683+masanson@users.noreply.github.com>
2023-08-27 09:04:16 +0000

    [Update] genre30%

---
+++ app/controllers/admin/genres_controller.rb
@@ -0,0 +1,25 @@
+class Admin::GenresController < ApplicationController
+  
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to admin_genres_path
+    end
+  end
+  
+  def index
+    @genres = Genre.all
+    @genre = Genre.new
+  end
+
+  def edit
+  end
+  
+  def update
+    
+  end
+  
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get '' => 'homes#top', as: 'top'
+    resources :genres, only: [:create, :index, :edit, :update]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
