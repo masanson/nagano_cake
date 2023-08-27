fcb58182290f8dcaa5bb99fa41a885d9a6e66e7b
masanson <138214683+masanson@users.noreply.github.com>
2023-08-27 12:12:43 +0000

    [Update] admin/items10%

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+
   root to: "public/homes#top"
   get '/about' => 'public/homes#about', as: 'about'
 
@@ -22,6 +23,7 @@ Rails.application.routes.draw do
   namespace :admin do
     get '' => 'homes#top', as: 'top'
     resources :genres, only: [:create, :index, :edit, :update]
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
