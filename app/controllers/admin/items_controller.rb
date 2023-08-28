6a823c2db0abf6a89f835a4baab871ef94818b9c
masanson <138214683+masanson@users.noreply.github.com>
2023-08-28 11:51:09 +0000

    [Update] admin/items15%_new作成中

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,8 +1,19 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item.id)
+    else
+      render :new
+    end
   end
 
   def show
@@ -10,4 +21,11 @@ class Admin::ItemsController < ApplicationController
 
   def edit
   end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:sweets_image, :name, :introduction, :genre_id, :price, :is_on_sale )
+  end
+  
 end
