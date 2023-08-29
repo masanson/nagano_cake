b931d52affd7da3b291c27654e16b25092c4a769
masanson <138214683+masanson@users.noreply.github.com>
2023-08-29 12:41:11 +0000

    [Update] admin/items80%_new/edit/index/show作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,8 +6,8 @@ class Item < ApplicationRecord
   def get_sweets_image(width, height)
     unless sweets_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'defauit-image.jpg', content_type: 'image/jpeg')
+      sweets_image.attach(io: File.open(file_path), filename: 'defauit-image.jpg', content_type: 'image/jpeg')
     end
-    profile_image.variant(resize_to_limit: [width, height]).processed
+    sweets_image.variant(resize_to_limit: [width, height]).processed
   end
 end
