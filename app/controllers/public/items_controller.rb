8ad28aada3106ed5b41d3b7697fa56a207d13418
masanson <138214683+masanson@users.noreply.github.com>
2023-08-30 11:25:02 +0000

    [Update] public/items80%_index/show作成

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,16 @@
 class Public::ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
   end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:sweets_image, :name, :introduction, :genre_id, :price, :is_on_sale )
+  end
+  
 end
