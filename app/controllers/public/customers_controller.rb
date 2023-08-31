1da7dd98ef06bfce88545d4404d2884a24ec8551
masanson <138214683+masanson@users.noreply.github.com>
2023-08-31 12:56:15 +0000

    [Update] 退会before_action,customer_state作成2

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -10,7 +10,7 @@ class Public::CustomersController < ApplicationController
   def update
     @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
-      redirect_to customer_path(@customer.id)
+      redirect_to public_customer_path(@customer.id)
     else
       render :edit
     end
@@ -21,10 +21,10 @@ class Public::CustomersController < ApplicationController
 
   def update_wd
   end
-  
+
   private
 
-  
+
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number)
   end
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -24,7 +24,7 @@ class Public::SessionsController < Devise::SessionsController
     @customer = Customer.find_by(email: params[:customer][:email])
     return if !@customer
     if @customer.valid_password?(params[:customer][:password])
-      if @customer.is_deleted = true
+      if @customer.is_deleted == true
         redirect_to new_customer_registration_path
       end
     end
