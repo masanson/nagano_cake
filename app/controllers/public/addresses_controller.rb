92f0c626ea534c51182a535e28178b66dfd2ce05
masanson <138214683+masanson@users.noreply.github.com>
2023-09-01 13:07:05 +0000

    [Update] public/addresses_18%

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,9 +1,17 @@
 class Public::AddressesController < ApplicationController
   def index
+    @addresses = Address.all
+    @address = Address.new
   end
 
   def create
-    
+    @address = Address.new(address_params)
+    if @address.save
+      redirect_to admin_addresses_path
+    else
+      @addresses = Address.
+      render :index
+    end
   end
 
   def edit
