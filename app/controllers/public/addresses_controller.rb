e84060da7f846fa5b4bbbf46b70dd03e32bb5fc9
masanson <138214683+masanson@users.noreply.github.com>
2023-09-02 06:47:42 +0000

    [Update] public/addresses_80%

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -6,28 +6,37 @@ class Public::AddressesController < ApplicationController
 
   def create
     @address = Address.new(address_params)
-    if @address.save
-      redirect_to admin_addresses_path
+    @address.customer_id = current_customer.id
+    if @address.save!
+      redirect_to public_addresses_path
     else
-      @addresses = Address.
+      @addresses = Address.all
       render :index
     end
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
   
   def update
-    
+    @address = Address.find(params[:id])
+    if @address.update(params[:id])
+      redirect_to public_addresses_path
+    else
+      render :edit
+    end
   end
   
   def destroy
-    
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to public_addresses_path
   end
   
   private
   
   def address_params
-    params.require(:address).permit(:postal_code, :address, :name)
+    params.require(:address).permit(:postal_code, :address, :name, :customer_id)
   end
 end
