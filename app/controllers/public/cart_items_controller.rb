897cca87e3c39093a72183bc7ad3d47373fc9230
masanson <138214683+masanson@users.noreply.github.com>
2023-09-03 12:28:44 +0000

    [Update] public/cart_item_20%

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,30 +1,33 @@
 class Public::CartItemsController < ApplicationController
   def index
     @cart_items = CartItem.all
+    @cart_item = CartItem.new
   end
-  
+
   def create
     @cart_item = CartItem.new(cart_item_params)
-    if @cart_item.save
-    end
+    @customer = current_customer.id
+    @cart_item.save
+    redirect_to public_cart_items_path
   end
-  
+
   def update
-    
+    @cart_item = CartItem.find(params[:id])
+    @cart_item = Cartitem.update(cart_item_params)
   end
-  
+
   def destroy
-    
+
   end
-  
+
   def empty
-    
+
   end
-  
+
   private
-  
+
   def cart_item_params
-    params.require(:cart_item).permit(:amount, :item_id, :customer_id)
+    params.require(:cart_item).permit(:amount, :item_id)
   end
-  
+
 end
