19aeeda2a820df425c2ab61a4ccf031412798265
masanson <138214683+masanson@users.noreply.github.com>
2023-09-11 09:09:44 +0000

    [Update] public/orders作成70%

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,29 +5,33 @@ class Public::OrdersController < ApplicationController
   
   def check
     @order = Order.new(order_params)
-    @order_detial = OrderDetial.new(order_detial_params)
-    if params[:order][:select_address] == 0
+    @cart_items = current_customer.cart_items
+    @total = 0
+    @shipping_charge = 800
+    if params[:order][:select_address] == "0"
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
       @order.name = current_customer.first_name + current_customer.last_name
-    elsif params[:order][:select_address] == 1
+    elsif params[:order][:select_address] == "1"
       @address = Address.find_by(params[:order][:address_id])
       @order.postal_code = @address.postal_code
       @order.address = @address.address
       @order.name = @address.name
-    elsif params[:order][:select_address] == 2
     end
-    render :check
-  end
-  
-  def complition
-
   end
   
   def create
     @order = Order.new(order_params)
     @order_detial = OrderDetial.new(order_detial_params)
-    # @orders = current_customer.order.
+    @cart_items = current_customer.cart_items   
+    @order_detial.price = @cart_items.item.with_tax_price
+    @order_detial.amount = @cart_items.amount
+    @order.shipping_charge = @shipping_charge
+    
+  end
+  
+  def complition
+
   end
   
   def index
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,5 @@ class Order < ApplicationRecord
   has_many :order_detials, dependent: :destroy
   
   enum payment_method: { credit_card: 0, transfar: 1 }
+
 end
