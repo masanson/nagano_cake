5e049627352ce93a4b38bb79c9d12b56521d62cc
masanson <138214683+masanson@users.noreply.github.com>
2023-09-17 11:38:40 +0000

    [Update] レイアウト_カスタマー新規登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,17 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource)
+  def after_sign_up_path_for(resource)
     case resource
     when Customer
       public_customer_path(current_customer)
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    case resource
+    when Customer
+      root_path
     when Admin
       admin_top_path
     end
