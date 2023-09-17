e11e89f13bc2cf5f93c7865cdae8ca49d0049c0d
masanson <138214683+masanson@users.noreply.github.com>
2023-09-17 11:56:32 +0000

    [Update] test_登録～注文_3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,6 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_up_path_for(resource)
-    case resource
-    when Customer
-      public_customer_path(current_customer)
-    end
-  end
-
   def after_sign_in_path_for(resource)
     case resource
     when Customer
