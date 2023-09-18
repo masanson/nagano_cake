ecd1fa5c7c8541e9821cb9677d95770591485389
masanson <138214683+masanson@users.noreply.github.com>
2023-09-18 05:40:56 +0000

    [Update] test_登録～注文_完了

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -13,7 +13,7 @@ class Public::OrdersController < ApplicationController
       @order.address = current_customer.address
       @order.name = current_customer.first_name + current_customer.last_name
     elsif params[:order][:select_address] == "1"
-      @address = Address.find_by(params[:order][:address_id])
+      @address = Address.find_by(id: params[:order][:address_id])
       @order.postal_code = @address.postal_code
       @order.address = @address.address
       @order.name = @address.name
